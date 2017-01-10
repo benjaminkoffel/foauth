97ba496177babc8ecc576e887060048ee5e328c6
Ben Koffel <bkoffel@seek.com.au>
2017-01-10 11:12:54 +1100

    add tests

--- foauth.py
+++ foauth.py
@@ -8,7 +8,7 @@
 # get access code for mismatching client and redirect uri
 # get acess code with various permutations of redirect uri to test for open redirection
 #
-# post access code with submitting any kind of CSRF check
+# post access code without submitting any kind of CSRF check
 # post access code for unregistered redirect uri
 # post access code for invalid scope
 # post access code for invalid client
@@ -29,10 +29,27 @@ import requests
 import argparse
 import urlparse
 import uuid
+import difflib
 
-def basic_auth():
+def generate_redirect_uri_permutations():
+    return [
+        'http://evil.com',
+        args.redirect_uri + '/',
+        args.redirect_uri + '/../',
+        args.redirect_uri + '%20',
+        args.redirect_uri + '%x20',
+        args.redirect_uri + '//',
+        args.redirect_uri + '//google.com',
+        args.redirect_uri + '&i=293'
+    ]
 
-    print('Basic Auth:'),
+def compare_content(content_1, content_2):
+    return difflib.SequenceMatcher(None, content_1, content_2).ratio() > 0.75
+
+#  NOTE: resource owner authentication will differ between implementations
+def authenticate_resource_owner():
+
+    print('Authenticate Resource Owner:'),
 
     basic_auth_response = requests.post(args.basic_auth_uri, auth=requests.auth.HTTPBasicAuth(args.resource_owner_id, args.resource_owner_secret), allow_redirects=False)
 
@@ -40,7 +57,7 @@ def basic_auth():
         print('  ERROR: could not authenticate resource owner')
         exit()
 
-    print('  OK')
+    print('  DONE')
 
     return basic_auth_response.cookies
 
@@ -50,13 +67,24 @@ def test_authorization_code_flow(cookies):
 
     state = str(uuid.uuid4())
 
-    get_authorize_response = requests.get('%s?response_type=code&client_id=%s&scope=%s&state=%s&redirect_uri=%s' % (args.authorize_uri, args.client_id, args.scope, state, args.redirect_uri), cookies=cookies, allow_redirects=False)
+    get_authorize_uri = '%s?response_type=code&client_id=%s&scope=%s&state=%s&redirect_uri=%s' % (args.authorize_uri, args.client_id, args.scope, state, args.redirect_uri)
+
+    get_authorize_response = requests.get(get_authorize_uri, cookies=cookies, allow_redirects=False)
 
     if get_authorize_response.status_code != 200:
         print('  FAIL - GET authorize returned invalid status code %s' % get_authorize_response.status_code)
         return
 
-    post_authorize_response = requests.post('%s?response_type=code&client_id=%s&scope=%s&state=%s&redirect_uri=%s' % (args.authorize_uri, args.client_id, args.scope, state, args.redirect_uri), data={'submit': 'Allow access'}, cookies=cookies, allow_redirects=False)
+    # get access code for unregistered redirect uri
+    for redirect_uri_ in generate_redirect_uri_permutations():
+        get_authorize_uri_ = '%s?response_type=code&client_id=%s&scope=%s&state=%s&redirect_uri=%s' % (args.authorize_uri, args.client_id, args.scope, state, redirect_uri_)
+        get_authorize_response_ = requests.get(get_authorize_uri_, cookies=cookies, allow_redirects=False)
+        if get_authorize_response_.status_code == 200 and compare_content(get_authorize_response.content, get_authorize_response_.content):
+            print('  WARN - can get access code for unregistered redirect uri %s' % redirect_uri_)
+            return
+
+    # NOTE: authorizing access will differ between implementations
+    post_authorize_response = requests.post(get_authorize_uri, data={'submit': 'Allow access'}, cookies=cookies, allow_redirects=False)
 
     if post_authorize_response.status_code != 302:
         print('  FAIL - POST authorize returned invalid status code %s' % post_authorize_response.status_code)
@@ -100,7 +128,10 @@ def test_authorization_code_flow(cookies):
     if post_token_response.json()['expires_in'] > 3600:
         print('  WARN - POST token response has long lived access token (%s seconds)' % post_token_response.json()['expires_in'])
 
-    print('  OK')
+    # also supports basic auth
+    #post_token_response = requests.post(args.token_uri, data={'grant_type': 'authorization_code', 'redirect_uri': args.redirect_uri, 'code': code}, auth=requests.auth.HTTPBasicAuth(args.client_id, args.client_secret), allow_redirects=False)
+
+    print('  DONE')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -115,5 +146,5 @@ if __name__ == '__main__':
     parser.add_argument('--resource-owner-id')
     parser.add_argument('--resource-owner-secret')
     args = parser.parse_args()
-    cookies = basic_auth()
+    cookies = authenticate_resource_owner()
     test_authorization_code_flow(cookies)
